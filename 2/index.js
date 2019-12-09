8fbdcef4500e9f57620766d343db2662476c97ca
Camron Flanders <camron.flanders@gmail.com>
2019-12-08 21:26:53 -0600

    formatting

--- 2/index.js
+++ 2/index.js
@@ -3,13 +3,14 @@ const input = JSON.parse(fs.readFileSync("./input.json").toString("utf8"))
 
 const debug = false
 const log = msg => {
-    if(!debug) { return }
+    if (!debug) {
+        return
+    }
     console.log(msg)
 }
 
 const sum = (x, y) => x + y
 const mult = (x, y) => x * y
-
 
 function main(inputs) {
     let _input = [...inputs]
@@ -22,11 +23,11 @@ function main(inputs) {
         // find inputs
         const x = _input[r1]
         const y = _input[r2]
-        log('inputs', x, y)
+        log("inputs", x, y)
 
         // get result and store it
         const result = fn(x, y)
-        log('output', result)
+        log("output", result)
         _input[r3] = result
 
         // increment our ptr
@@ -36,18 +37,19 @@ function main(inputs) {
     const instructions = {
         1: /* add */ operation(sum),
         2: /* multiply */ operation(mult),
-        99: /* halt */ () => { return inputs.length + 1}
+        99: /* halt */ () => {
+            return inputs.length + 1
+        },
     }
 
     let ptr = 0
-    while(ptr < _input.length) {
+    while (ptr < _input.length) {
         const i = _input[ptr]
         ptr = instructions[i](ptr)
     }
 
     return _input[0]
 }
-
 
 // part 1
 input[1] = 12
@@ -60,13 +62,13 @@ console.log("Part1", main(input))
 const makeRange = max => new Array(max).fill(1).map((_, idx) => idx)
 
 function findNounAndVerb(inputs) {
-    for(let x of makeRange(99)) {
-        for(let y of makeRange(99)) {
+    for (let x of makeRange(99)) {
+        for (let y of makeRange(99)) {
             let _input = [...inputs]
             _input[1] = x
             _input[2] = y
 
-            if(main(_input) === 19690720) {
+            if (main(_input) === 19690720) {
                 return [x, y]
             }
         }
@@ -74,6 +76,5 @@ function findNounAndVerb(inputs) {
 }
 
 console.log("Part2", findNounAndVerb(input))
-
 
 process.exit(0)
